8735de961bd1f99160031a5fead1c5ffeede5730
tran-matt <143528178+tran-matt@users.noreply.github.com>
2025-02-15 17:01:04 -0500

    deleted frontend folder

--- frontend/src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://dqnoznfkjkexgtzvjpcq.supabase.co';  
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxbm96bmZramtleGd0enZqcGNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg5NTQ4MjAsImV4cCI6MjA1NDUzMDgyMH0.nP8Zloowi4mz7MkB9ggYkcn9cady3yk9IbDsCrdhGS4';  // Replace with your public API key
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
