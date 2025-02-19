ee9f7535a0328465186493220fde72d3fcef7891
tran-matt <143528178+tran-matt@users.noreply.github.com>
2025-02-19 03:20:43 -0500

    Added icons

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Supabase credentials
-const supabaseUrl = 'https://dqnoznfkjkexgtzvjpcq.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxbm96bmZramtleGd0enZqcGNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg5NTQ4MjAsImV4cCI6MjA1NDUzMDgyMH0.nP8Zloowi4mz7MkB9ggYkcn9cady3yk9IbDsCrdhGS4';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
